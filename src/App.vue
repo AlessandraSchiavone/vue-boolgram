4e10a8701d8f99232a5823befcf19ed0ae1bcaca
AlessandraSchiavone <alessandraschiavone31@gmail.com>
2021-09-22 12:36:51 +0200

    add Component Nav

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,31 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Nav />
+    <main>
+      <Left />
+      <Right />
+    </main>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Nav from './components/Nav';
+import Left from './components/Left';
+import Right from './components/Right';
+
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Nav,
+    Left,
+    Right
   }
 }
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
+@import './style/general';
+
 </style>
---
+++ src/components/Nav.vue
@@ -0,0 +1,68 @@
+<template>
+    <header>
+        <div class="nav container d-flex justify-content-around align-items-center">
+            <div class="logo">
+                <img src="../assets/logo.png" alt="Logo Boolgram">
+            </div>
+            <div class="search "> Cerca </div>
+            <div class="info-account d-flex justify-content-around align-items-center">
+                <i class="far fa-heart mx-2"></i>
+                <i class="fas fa-home  mx-2 "></i>
+                <div class="circle text-center mx-2"></div>
+            </div>  
+        </div>    
+    </header>   
+       
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+header{
+    height:60px;
+    width:100%;
+    border-bottom:1px solid lightgrey;
+    position:fixed;
+    top:0;
+    left:0; 
+    .nav{
+        height:100%;
+        .logo{
+            img{
+               height:40px;    
+            }
+            &:hover{
+                    cursor:pointer;
+                } 
+        }
+        .search{
+           border: 1px solid lightgrey;
+           padding:2px 80px;
+           color:grey;
+           font-weight: 500;
+        }
+        .info-account{
+            i{
+                font-size:22px;
+                &:hover{
+                    cursor:pointer;
+                }
+            }
+            .circle{
+                width:40px;
+                height:40px;
+                border-radius:50%;
+                background-image: url(../assets/profile.jpg);
+                background-size: contain;
+                &:hover{
+                    cursor:pointer;
+                }
+            }
+        }
+    }  
+}
+</style>
